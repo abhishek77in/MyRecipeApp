308ba451068ee61bc53c58f341aa1a8dcf301595
Abhishek Srivastava <abhishek77in@gmail.com>
2025-02-09 00:48:38 +0530

    fetch categories

--- app/src/main/java/com/labsmartlis/myrecipeapp/MainViewModel.kt
+++ app/src/main/java/com/labsmartlis/myrecipeapp/MainViewModel.kt
@@ -1,8 +1,34 @@
 package com.labsmartlis.myrecipeapp
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
+
+    private val _categoriesState = mutableStateOf(RecipeState())
+    val categoriesState: State<RecipeState> = _categoriesState
+
+    private fun fetchCategories(){
+        viewModelScope.launch {
+            try {
+                val response = recipeService.getCategories()
+                _categoriesState.value = _categoriesState.value.copy(
+                    list = response.categories,
+                    loading = false,
+                    error = null
+                )
+
+            } catch (e: Exception) {
+                _categoriesState.value = _categoriesState.value.copy(
+                    loading = false,
+                    error = "Error fetching categories: ${e.message}"
+                )
+            }
+        }
+    }
 
     data class RecipeState(
         val loading: Boolean = true,
