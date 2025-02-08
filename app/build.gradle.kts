7035637e0aefb60fde5f66be18395cb186aa7724
Abhishek Srivastava <abhishek77in@gmail.com>
2025-02-09 00:15:38 +0530

    Add image dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,9 @@ dependencies {
     //Json to Kotlin object mapping
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
+    //Image loading
+    implementation("io.coil-kt:coil-compose:2.4.0")
+
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.activity:activity-compose:1.10.0")
