4a576d7aa47a9d59a642acf90314be9f4d2904ac
Abhishek Srivastava <abhishek77in@gmail.com>
2025-02-10 00:20:17 +0530

    Update module setting and permission

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.labsmartlis.myrecipeapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.labsmartlis.myrecipeapp"
